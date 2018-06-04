031a0429443d79751594cc8575dfe0e0bb0009ec
Deva <devanshverma951@gmail.com>
2018-06-04 23:53:32 +0530

    PWA and Nodejs

--- sw.js
+++ sw.js
@@ -1,18 +1,42 @@
 const staticAssetss = [
-    './',
-    './css/style.css',
-    './js/app.js',
-    './fallback.json',
-    './images/fetch-dog.jpg'
+    '/',
+    '../css/style.css',
+    '/js/fallback.json',
+    '/js/view.js',
+    '../images/fetch-dog.jpg'
 ];
 
+// ################# Install #################
 self.addEventListener('install',async event =>{
     const cache = await caches.open('news-static');
     cache.addAll(staticAssetss);
 
     console.log("Installed!");
+    // self.skipWaiting();
 });
 
+// ################# Activate #################
+self.addEventListener('activate', (event) => {
+    console.log('Event: Activate');
+
+    //Remove old and unwanted caches
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== "news-static") {     //cacheName = 'cache-v1'
+                        return caches.delete(cache);    //Deleting the cache
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+//
+
+// ################# Fetch #################
 self.addEventListener('fetch',event =>{
     const req = event.request;
     const url = new URL(req.url);
@@ -22,9 +46,10 @@ self.addEventListener('fetch',event =>{
         // console.log("Cache First");
     }else{
         event.respondWith(networkFirst(req));
-        // console.log("Network First");
+        console.log("Network First");
     }
 
+    // console.log("Fetch Request");
 });
 
 async function cacheFirst(req) {
@@ -34,16 +59,27 @@ async function cacheFirst(req) {
 }
 
 async function networkFirst(req) {
+    // Make Dynamic content
     const dynamicCache = await caches.open('news-dynamic');
 
     try{
+        // If Online...Send Network Response
         const res = await fetch(req);
         dynamicCache.put(req,res.clone());
         return res;
     }catch (e) {
+        // If Offline...Send Cached result or fallback.json file
+        console.log("fallback/cache--servise-worker");
         const cachedResults = await dynamicCache.match(req);
+        if (cachedResults){
+            console.info("Dynamic Cache");
+            return cachedResults;
+        }else{
+            console.info("fallback.json");
+            return await caches.match('/js/fallback.json');
+        }
 
-        return cachedResults || await caches.match('./fallback.json');
     }
 
 }
+
